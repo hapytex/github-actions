5f718c88dd03a68a4db463f3cb4b5789f53a3e77
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-03-27 11:13:23 +0100

    add a CNAME action

--- actions/new-action.sh
+++ actions/new-action.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-mkdir -p "$1"
-cp action.yml.template "$1/action.yml"
-editor "$1/action.yml"
+name=$(echo "$@")
+
+# source: https://gist.github.com/oneohthree/f528c7ae1e701ad990e6
+slug=$(iconv -t ascii//TRANSLIT <<<"$name" | sed -r 's/[^a-zA-Z0-9]+/-/g' | sed -r 's/^-+\|-+$/-/g' | tr A-Z a-z)
+
+fn="$slug/action.yml"
+
+mkdir -p "$slug"
+printf "$(< action.yml.template)" "$name" > "$fn"
+editor "$fn"
+git add "$fn"
+git commit -am "Add an action for $name"
